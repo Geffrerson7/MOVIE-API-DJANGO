baf37c2e5b28428e74884c834be91ce96775006d
Gefferson Casasola <gcasasolah@uni.pe>
2023-03-03 13:21:24 -0500

    Add rent and user app with jwt

--- movies/settings.py
+++ movies/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'movie',
-    'drf_yasg'
+    'drf_yasg',
+    'user',
+    'rent',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+    
+}
--- rent/urls.py
+++ rent/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router=DefaultRouter()
-router.register('', views.MovieViewSet,basename="movie")
+router.register('', views.RentViewSet, basename="rent")
 urlpatterns = [
     path('', include(router.urls))
 ]
